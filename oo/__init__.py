b2ebd6f442801088acf0debdb8d12cd0b22f508c
Wesley Silva <wesleyfigurinha@gmail.com>
2021-05-06 20:28:52 -0300

    Criados atributos name and year

--- oo/__init__.py
+++ oo/__init__.py
@@ -1,4 +1,8 @@
 class Person:
+    def __init__(self, name=None, year=0):
+        self.year = year
+        self.name = name
+
     def cumprimentar(self):
         return 'Hello'
 
@@ -6,3 +10,5 @@ class Person:
 if __name__ == '__main__':
     p = Person()
     print(p.cumprimentar())
+    print(p.year)
+    print(p.name)
