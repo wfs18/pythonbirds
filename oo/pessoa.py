c21999870f7940696507b7b28331e4eb8ee2df48
Wesley Silva <wesleyfigurinha@gmail.com>
2021-05-07 22:30:25 -0300

    Criado e deltado

---
+++ oo/pessoa.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self, *children, name=None, year=0):
+        self.year = year
+        self.name = name
+        self.children = list(children)
+
+    def cumprimentar(self):
+        return 'Hello'
+
+
+if __name__ == '__main__':
+    p = Person()
+    eu = Person(name='marcio')
+    wes = Person(eu, name='Wesley')
+    print(p.cumprimentar())
+    print(p.year)  # Atributo de instancia
+    print(p.name)  # Atributo de dados
+    for filhos in wes.children:
+        print(filhos.year)
+    p.sobre = 'eu'
+    del p.sobre
+    print(p.sobre)
